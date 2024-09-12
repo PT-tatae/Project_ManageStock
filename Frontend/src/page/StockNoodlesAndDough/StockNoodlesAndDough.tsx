a97775bbe200a63cd499f59bae5ce856ef2ede04
jarntae <poramet2022@gmail.com>
2024-09-13 02:14:08 +0700

    Update StockNoodlesAndDough.tsx

--- Frontend/src/page/StockNoodlesAndDough/StockNoodlesAndDough.tsx
+++ Frontend/src/page/StockNoodlesAndDough/StockNoodlesAndDough.tsx
@@ -1,37 +1,15 @@
 // StockNoodlesAndDough.js
 
 import StockCategory from "../../Compornent/StockCategory/StockCategory";
-
-const noodlesAndDoughData = [
-  {
-    key: '1',
-    codelist: '1',
-    code: 'N001',
-    name: 'มาม่า',
-    quantity: '20',
-    price: '30',
-    supplier: 'Supplier A',
-    importDate: '2024-08-10 10:00',
-    expiryDate: '2025-08-10 10:00',
-  },
-  {
-    key: '2',
-    codelist: '2',
-    code: 'N002',
-    name: 'เส้นหมี่',
-    quantity: '15',
-    price: '50',
-    supplier: 'Supplier B',
-    importDate: '2024-08-12 15:00',
-    expiryDate: '2025-08-12 15:00',
-  }
-];
+import useStockData from "../../Hook/useStockData";
 
 export default function StockNoodlesAndDough() {
+  const noodlesAndDoughData = useStockData(4);
   return (
     <StockCategory
       categoryTitle="เส้นและแป้ง (Noodles and Dough)"
       initialData={noodlesAndDoughData}
+      categoryID = {4}
     />
   );
 }
