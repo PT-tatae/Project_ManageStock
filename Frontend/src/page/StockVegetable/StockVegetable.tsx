012af23e6b61cf5030ea651e7c9d326651f8d341
jarntae <poramet2022@gmail.com>
2024-09-13 02:14:14 +0700

    Update StockVegetable.tsx

--- Frontend/src/page/StockVegetable/StockVegetable.tsx
+++ Frontend/src/page/StockVegetable/StockVegetable.tsx
@@ -1,36 +1,15 @@
-
+import useStockData from "../../Hook/useStockData";
 import StockCategory from "../../Compornent/StockCategory/StockCategory";
 
-const meatData = [
-    {
-        key: '1',
-        codelist: '1',
-        code: 'V001',
-        name: 'คะน้า',
-        quantity: '20',
-        price: '50',
-        supplier: 'Supplier C',
-        importDate: '2024-08-01 08:00',
-        expiryDate: '2024-08-10 23:59',
-    },
-    {
-        key: '2',
-        codelist: '2',
-        code: 'V002',
-        name: 'ผักบุ้ง',
-        quantity: '15',
-        price: '30',
-        supplier: 'Supplier D',
-        importDate: '2024-08-07 09:00',
-        expiryDate: '2024-08-14 23:59',
-    }
-];
 
-export default function StockMeat() {
+
+export default function StockVegetable() {
+  const VegetableData = useStockData(2);
   return (
     <StockCategory
       categoryTitle="ผัก (Vegetables)"
-      initialData={meatData}
+      initialData={VegetableData}
+      categoryID = {2}
     />
   );
 }
