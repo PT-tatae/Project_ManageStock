bec6796516cc2fbf3c68bf205d97b0c34ba71e36
jarntae <poramet2022@gmail.com>
2024-09-13 02:14:04 +0700

    Update StockCondimentsAndSauce.tsx

--- Frontend/src/page/StockCondimentsAndSauce/StockCondimentsAndSauce.tsx
+++ Frontend/src/page/StockCondimentsAndSauce/StockCondimentsAndSauce.tsx
@@ -1,37 +1,16 @@
 // StockCondimentsAndSauce.js
-
+import useStockData from "../../Hook/useStockData";
 import StockCategory from "../../Compornent/StockCategory/StockCategory";
 
-const condimentsAndSauceData = [
-  {
-    key: '1',
-    codelist: '1',
-    code: 'C001',
-    name: 'น้ำจิ้มสุกกี้',
-    quantity: '12',
-    price: '100',
-    supplier: 'Supplier C',
-    importDate: '2024-08-15 11:00',
-    expiryDate: '2025-08-15 11:00',
-  },
-  {
-    key: '2',
-    codelist: '2',
-    code: 'C002',
-    name: 'น้ำมันงา',
-    quantity: '8',
-    price: '120',
-    supplier: 'Supplier D',
-    importDate: '2024-08-18 16:00',
-    expiryDate: '2025-08-18 16:00',
-  }
-];
+
 
 export default function StockCondimentsAndSauce() {
+  const condimentsAndSauceData = useStockData(5);
   return (
     <StockCategory
       categoryTitle="เครื่องปรุงรสและน้ำจิ้ม (Condiments and Sauce)"
       initialData={condimentsAndSauceData}
+      categoryID = {5}
     />
   );
 }
