b3495a579f04a4347e65a4f6b976358d4fda21ad
jarntae <poramet2022@gmail.com>
2024-09-13 02:14:06 +0700

    Update StockMeat.tsx

--- Frontend/src/page/StockMeat/StockMeat.tsx
+++ Frontend/src/page/StockMeat/StockMeat.tsx
@@ -1,37 +1,16 @@
 // StockMeat.js
-
+import useStockData from "../../Hook/useStockData";
 import StockCategory from "../../Compornent/StockCategory/StockCategory";
 
-const meatData = [
-  {
-    key: '1',
-    codelist: '1',
-    code: 'M001',
-    name: 'เนื้อหมู',
-    quantity: '10',
-    price: '150',
-    supplier: 'Supplier A',
-    importDate: '2024-08-01 14:00',
-    expiryDate: '2024-08-15 20:00',
-  },
-  {
-    key: '2',
-    codelist: '2',
-    code: 'M002',
-    name: 'เนื้อวัว',
-    quantity: '5',
-    price: '300',
-    supplier: 'Supplier B',
-    importDate: '2024-08-05 00:00',
-    expiryDate: '2024-08-20 23:59',
-  }
-];
+
 
 export default function StockMeat() {
+  const meatData = useStockData(1);
   return (
     <StockCategory
       categoryTitle="เนื้อสัตว์ (Meats)"
       initialData={meatData}
+      categoryID = {1}
     />
   );
 }
