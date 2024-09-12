432e6751670de841f8ba3b58a3b6a02e1eaf65b7
jarntae <poramet2022@gmail.com>
2024-09-12 16:10:36 +0700

    Update StockBeveragesAndDesserts.tsx

--- Frontend/src/page/StockBeveragesAndDesserts/StockBeveragesAndDesserts.tsx
+++ Frontend/src/page/StockBeveragesAndDesserts/StockBeveragesAndDesserts.tsx
@@ -7,11 +7,11 @@ import useStockData from "../../Hook/useStockData";
 export default function StockBeveragesAndDesserts() {
   const beveragesAndDessertsData = useStockData(6); // ดึงข้อมูล Stock ด้วย categoryID = 6
   //const SupplierData = useSupplierData(); // ดึงข้อมูล Supplier
-
   return (
     <StockCategory
       categoryTitle="เครื่องดื่มและขนมหวาน (Beverages and Desserts)"
       initialData={beveragesAndDessertsData}
+      categoryID = {6}
     />
   );
 }
